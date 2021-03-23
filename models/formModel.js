ab8fee0da903891f4029d0beedbe27042c5600db
Ata Shaikh <atasteven49@gmail.com>
2021-03-23 15:31:28 +0530

    Few changes

--- models/formModel.js
+++ models/formModel.js
@@ -5,9 +5,6 @@ const formSchema = new mongoose.Schema(
       type: Boolean,
       default: true,
     },
-    outOfMark: {
-      type: Number,
-    },
     formName: {
       type: String,
       required: true,

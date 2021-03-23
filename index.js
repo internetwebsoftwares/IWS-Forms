fa025ddbdec2d55de661ab13ac02110f2f668d2e
Ata Shaikh <atasteven49@gmail.com>
2021-03-23 22:06:30 +0530

    Few changes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
   res.header("Access-Control-Allow-Headers", "*");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");

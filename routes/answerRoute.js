4026b9d57e42c89b66b043d59c6fbf1a8c8a5f42
Ata Shaikh <atasteven49@gmail.com>
2021-03-23 22:49:53 +0530

    Few changes

--- routes/answerRoute.js
+++ routes/answerRoute.js
@@ -79,7 +79,7 @@ router.get("/answer/:id/answer", auth, async (req, res) => {
 });
 
 // Check the answers
-router.post("/answer/:id/check", auth, async (req, res) => {
+router.patch("/answer/:id/check", auth, async (req, res) => {
   let { totalMarks } = req.body;
   try {
     const answer = await Answer.findOne({
